83cea8dbc1e1152823d89e1aab0704a5d6a9b465
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-05-20 00:42:43 +1200

    Add DDD style classes

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,14 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Services
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
 
 var app = builder.Build();
 
---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+public interface IEntity
+{
+    public DateTime? CreatedAt { get; set; }
+    public string CreatedBy { get; set; }
+    public string LastModifiedAt { get; set; }
+    public string LastModifiedBy { get; set; }
+}
+
+public interface IEntity<T> : IEntity
+{
+    public T Id { get; set; }
+}
