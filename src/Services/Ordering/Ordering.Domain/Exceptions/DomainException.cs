7c70a73db481f1696e7dc14bb16ecaf17476d4b6
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-05-20 13:50:29 +1200

    Add Domain events

--- src/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
+++ src/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
@@ -1,17 +1,16 @@
-﻿namespace Ordering.Domain.Exceptions
+﻿namespace Ordering.Domain.Exceptions;
+
+public class DomainException : Exception
 {
-    public class DomainException : Exception
+    public DomainException()
     {
-        public DomainException()
-        {
-        }
+    }
 
-        public DomainException(string? message) : base(message)
-        {
-        }
+    public DomainException(string? message) : base(message)
+    {
+    }
 
-        public DomainException(string? message, Exception? innerException) : base(message, innerException)
-        {
-        }
+    public DomainException(string? message, Exception? innerException) : base(message, innerException)
+    {
     }
 }
