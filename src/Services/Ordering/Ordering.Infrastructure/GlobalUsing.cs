a29299e11bbefec58fb51ada6465fd9c4d537c9b
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-05-22 22:45:34 +1200

    Add DbContext and configurations

---
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+global using System.Reflection;
+global using Ordering.Domain.Enums;
