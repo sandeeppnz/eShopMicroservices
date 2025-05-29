86c66ee9bb5faa9fecd489b9ba7a39723d20a1f1
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-05-30 00:25:17 +1200

    Enhance MediatR configuration with new behaviors
    Updated `DependencyInjection.cs` to include `ValidationBehavior<,>` and `LoggingBehavior<,>` in the MediatR pipeline. Added necessary `using Common.BuildingBlocks.Behaviors` directive for proper behavior registration.

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,11 +1,15 @@
-﻿namespace Ordering.Application
+﻿using Common.BuildingBlocks.Behaviors;
+
+namespace Ordering.Application
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            services.AddMediatR(c => {
-                c.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(config => {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                config.AddOpenBehavior(typeof(LoggingBehavior<,>));
             });
             return services;
         }
