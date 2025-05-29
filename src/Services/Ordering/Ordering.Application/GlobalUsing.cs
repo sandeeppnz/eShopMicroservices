fc34da85d3d57a101df3d042183a130c1857caef
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-05-29 23:01:17 +1200

    C# feat(ordering): implement CreateOrderHandler to handle order creation
    - Added logic to map OrderDto to domain entities (Order, Address, Payment, OrderItem)
    - Persist new orders and their items using IApplicationDbContext
    - Returns the created order's ID as a result

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,2 +1,7 @@
 ﻿global using Microsoft.Extensions.DependencyInjection;
 global using System.Reflection;
+global using Common.BuildingBlocks.CQRS;
+global using Ordering.Application.Data;
+global using Ordering.Application.Dtos;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
--- src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -1,12 +1,48 @@
-﻿using Common.BuildingBlocks.CQRS;
+﻿namespace Ordering.Application.Orders.Commands.CreateOrder;
 
-namespace Ordering.Application.Orders.Commands.CreateOrder;
-
-public class CreateOrderHandler
+public class CreateOrderHandler(IApplicationDbContext dbContext)
     : ICommandHandler<CreateOrderCommand, CreateOrderResult>
 {
-    public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    public async Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var order = CreateNewOrder(command.Order);
+        dbContext.Orders.Add(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new CreateOrderResult(order.Id.Value);
+    }
+
+    private Order CreateNewOrder(OrderDto orderDto)
+    {
+        var shippingAddress = Address.Of(orderDto.ShippingAddress.FirstName, orderDto.ShippingAddress.LastName, orderDto.ShippingAddress.EmailAddress, orderDto.ShippingAddress.AddressLine, orderDto.ShippingAddress.Country, orderDto.ShippingAddress.State, orderDto.ShippingAddress.Postcode);
+        var billingAddress = Address.Of(orderDto.BillingAddress.FirstName, orderDto.BillingAddress.LastName, orderDto.BillingAddress.EmailAddress, orderDto.BillingAddress.AddressLine, orderDto.BillingAddress.Country, orderDto.BillingAddress.State, orderDto.BillingAddress.Postcode);
+
+        var payment = Payment.Of(
+            orderDto.Payment.CardName,
+            orderDto.Payment.CardNumber,
+            orderDto.Payment.Expiration,
+            orderDto.Payment.Cvv,
+            orderDto.Payment.PaymentMethod
+        );
+
+        var newOrder = Order.Create(
+            OrderId.Of(Guid.NewGuid()),
+            CustomerId.Of(orderDto.CustomerId),
+            OrderName.Of(orderDto.OrderName),
+            shippingAddress,
+            billingAddress,
+            payment
+        );
+
+        foreach (var item in orderDto.OrderItems)
+        {
+            newOrder.Add(
+                ProductId.Of(item.ProductId),
+                item.Quantity,
+                item.Price
+            );
+        }
+
+        return newOrder;
     }
 }
