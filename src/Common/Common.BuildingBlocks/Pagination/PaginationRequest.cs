ef67d375a28c2d27db1ab534b26f630e8add7557
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-06-03 20:30:54 +1200

    Refactor pagination in order queries
    Removed folder reference for GetOrders query. Added
    PaginatedResult<TEntity> class and PaginationRequest record
    to support pagination. Updated GetOrdersQuery and
    implemented GetOrdersHandler to handle paginated order
    retrieval from the database.

---
+++ src/Common/Common.BuildingBlocks/Pagination/PaginationRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace Common.BuildingBlocks.Pagination;
+
+public record PaginationRequest(int PageIndex, int PageSize);
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,33 @@
+﻿using Common.BuildingBlocks.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public record GetOrdersQuery(PaginationRequest PaginationRequest)
+    : IQuery<GetOrdersResult>;
+
+public record GetOrdersResult(PaginatedResult<OrderDto> Orders);
+
+
+public class GetOrdersHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+{
+    public async Task<GetOrdersResult> Handle(GetOrdersQuery request, CancellationToken cancellationToken)
+    {
+        var pageIndex = request.PaginationRequest.PageIndex;
+        var pageSize = request.PaginationRequest.PageSize;
+        var totalCount = await dbContext.Orders.LongCountAsync(cancellationToken);
+
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .OrderBy(o => o.OrderName.Value)
+            .Skip(pageIndex * pageSize)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersResult(new PaginatedResult<OrderDto>(
+            pageIndex,
+            pageSize,
+            totalCount,
+            orders.ToOrderDtoList()));
+    }
+}
