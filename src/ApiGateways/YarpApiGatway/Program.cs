bd8c08d8f51121f3da9e1be10c4b7a6bbcd991a0
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-06-07 21:51:42 +1200

    Add rate limiting and update YARP API gateway configuration
    - Implemented rate limiting in `Program.cs` with a fixed window limiter.
    - Updated `YarpApiGatway.csproj` to include a Docker Compose project path.
    - Defined new reverse proxy routes in `appsettings.json` with rate limiter policy.
    - Enhanced `docker-compose.override.yml` with environment variables and service dependencies.
    - Added YARP API gateway service to `docker-compose.yml`.
    - Modified `launchSettings.json` to include YARP API gateway for debugging.
    - Created `appsettings.Local.json` for local logging and reverse proxy settings.

--- src/ApiGateways/YarpApiGatway/Program.cs
+++ src/ApiGateways/YarpApiGatway/Program.cs
@@ -1,14 +1,25 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.PermitLimit = 5; // Maximum number of requests
+        options.Window = TimeSpan.FromSeconds(10); // Time window for the limit
+    });
+});
 
 var app = builder.Build();
 
 
 // Configure HTTP request pipeline
+app.UseRateLimiter();
 app.MapReverseProxy();
 
 
