c282ab7832c67396dae7e035559f562dbf001dfc
Sandeep Perera <Sandeep.Perera@fastway.co.nz>
2025-06-13 21:13:12 +1200

    Add pages

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,20 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shopping.Web.Models.Catalog;
+using Shopping.Web.Services;
 
 namespace Shopping.Web.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel(ICatalogService catalogService, ILogger<IndexModel> logger)
+        : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IEnumerable<ProductModel>  ProductList { get; set; } = new List<ProductModel>();
+
+        public async Task<IActionResult> OnGetAsync()
         {
-            _logger = logger;
-        }
+            try
+            {
 
-        public void OnGet()
-        {
+                logger.LogInformation("Index page visited.");
+                //var result = await catalogService.GetProducts(2,3);
+                var result = await catalogService.GetProducts();
 
+                ProductList = result.Products;
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("Inner: " + ex.InnerException?.Message);
+                throw;
+            }
         }
     }
 }
